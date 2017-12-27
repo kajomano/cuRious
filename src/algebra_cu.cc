539a123bfdecc09a3ccb8047534e7e74300bf1e3
marton.kajo <marton.kajo.ext@nokia-bell-labs.com>
2017-12-27 21:54:06 +0100

    Tuned the add kernel to max performance Added script Test/003_benchmark_vector_add.R to test speedup Spoiler alert: 4x speedup on the worst possible hardware, definitely worth it

--- src/algebra_cu.cc
+++ src/algebra_cu.cc
@@ -8,14 +8,19 @@
 __global__
 void elem_wise_add_global( float* dev_l, float* dev_r, int l, float* dev_res )
 {
-  for(int i = 0; i < l; i++){
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
+  for (int i = index; i < l; i += stride)
     dev_res[i] = dev_l[i] + dev_r[i];
-  }
 }
 
 extern "C"
 void elem_wise_add_host(  float* dev_l, float* dev_r, int l, float* dev_res ){
-  elem_wise_add_global<<<1, 1>>>(dev_l, dev_r, l, dev_res);
-  cudaDeviceSynchronize();
 
+  int blockSize = 256;
+  int numBlocks = (l + blockSize - 1) / blockSize;
+
+  elem_wise_add_global<<<numBlocks, blockSize>>>(dev_l, dev_r, l, dev_res);
+
+  cudaDeviceSynchronize();
 }

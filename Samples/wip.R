ee783741895d70c750ef76f5dae21d6bb39d29a3
kajomano <kajomano@hotmail.com>
2018-05-25 10:56:04 +0200

    Added NAMED scripts to wip.R Proper guards against unintentional overwrites can not be implemented, until reference counting is the standard for lazy copying in R.

--- Samples/wip.R
+++ Samples/wip.R
@@ -1,13 +1,39 @@
 library( cuRious )
+library( R6 )
 library( microbenchmark )
 
-var1 <- 1
+fun <- function( var ){
+  var + 1
+}
 
-.Call( "cuR_is_only_reference", 1+0 )
+test.class <- R6Class(
+  "test",
+  public = list(
+    ptr = NULL,
+    ref = function(){
+      .Call( "cuR_count_references", self$ptr )
+    }
+  ),
 
+  active = list(
+    set = function( var ){
+      print( .Call( "cuR_count_references", var ) )
+      self$ptr <- var
+    }
+  )
+)
 
-.Internal(inspect(1))
-var2 <- var1
-.Internal(inspect(var1))
-var3 <- var1
-.Internal(inspect(var3))
+test <- test.class$new()
+
+var <- 1:3
+test$set <- fun( var )
+
+.Call( "cuR_count_references", var )
+
+test$ref()
+test$ptr <- fun( 1 )
+test$ptr <- 1:3
+
+.Call( "cuR_count_references", 1 )
+.Call( "cuR_count_references", 1:3 )
+.Call( "cuR_count_references", fun(1) )
